566723ccfc20ed2190740d9f4724730b05301b72
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-29 20:51:09 -0500

    First Commit

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pickANumber = 1;
+
+
+            do
+            {
+                Console.WriteLine(pickANumber);
+                pickANumber++;
+            } while (pickANumber <= 100);
+
+                Console.ReadKey();
+        }
+    }
+}
